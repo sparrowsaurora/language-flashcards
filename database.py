8c1f614e0867cc24da71cd19474c0edeaccc4272
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-01-27 13:09:12 +0800

    struct: added function primatives

--- database.py
+++ database.py
@@ -2,8 +2,33 @@ class DatabaseLayer:
     '''
     DAL for Main app
     '''
-    def __init__():
+
+    def __init__(self):
         '''
         Init local db connection
         '''
         pass
+
+    def add(self, english: str, deutsch: str):
+        raise NotImplementedError
+
+    def read(self, english: str) -> tuple:
+        '''
+        returns deutsch translation
+        '''
+        raise NotImplementedError
+
+    def browse(self):
+        raise NotImplementedError
+
+    def delete(self):
+        raise NotImplementedError
+
+    def edit(self):
+        raise NotImplementedError
+
+    def random_word(self, english=True):
+        '''
+        call read on random word
+        '''
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from database import DatabaseLayer
+from enum import Enum
+
+
+class RoutineLevels(Enum):
+    BEGINNER = 20
+    INTERMEDIATE = 50
+    HIGH = 90
 
 
 class Main:
@@ -6,9 +13,32 @@ class Main:
     Main app entry (call on db for flash cards)
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         '''
         init DB and start vocab
         '''
-        DB = DatabaseLayer()
+        self.DB = DatabaseLayer()
         pass
+
+    def level(self) -> bool:
+        '''
+        level function
+        pass = true
+        fail = false
+        '''
+        return True
+
+    def check(self, word: str, english: bool = True) -> bool:
+        return True
+
+    def routine(self, level: RoutineLevels):
+        '''
+        main dev point
+        the actual program that asks for input
+
+        level = no. of words to complete
+        '''
+
+    def random(self):
+        random_word = self.DB.random_word()
+        return random_word
